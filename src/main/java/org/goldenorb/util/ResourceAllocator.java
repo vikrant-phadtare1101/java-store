903ca3a2dd7fc18eed5398b0c22c1568b233dd6e
vikrant-phadtare1101 <vikrant.phadtare@acellere.com>
2023-12-21 19:24:54 +0530

    Update ResourceAllocator.java

--- src/main/java/org/goldenorb/util/ResourceAllocator.java
+++ src/main/java/org/goldenorb/util/ResourceAllocator.java
@@ -58,11 +58,7 @@ public class ResourceAllocator<M extends OrbTrackerMember> {
   public ResourceAllocator(OrbConfiguration conf, Collection<M> orbTrackers) {
     this.setConf(conf);
     this.setOrbTrackers(orbTrackers);
-    System.out.println("code issue added");
-     System.out.println("code issue added");
-     System.out.println("code issue added");
-     System.out.println("code issue added");
-     System.out.println("code issue added");
+    
   }
   
   /**
